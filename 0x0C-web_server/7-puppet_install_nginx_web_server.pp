a9c4254b87e6930fa6d9feeb30ecf27b280db8fe
el-dani-cortes <danicor83@gmail.com>
2021-04-06 23:19:23 +0000

    UPDATED | Changed the way to declare the manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,31 @@
 # Practice configuring a server with Puppet! Just as you did before, 
 # install and configure an Nginx server using Puppet instead of Bash. To save time and effort, include resources in 
 # the manifest to perform a 301 redirect when querying /redirect_me.
-include stdlib
+# Manifest to configure an Ubuntu server with nginx
 
-# Install nginx package
-exec { 'Update':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => 'shell',
+# Install nginx
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
-->
-package { 'Nginx web server':
-  name    => 'nginx',
-}
-->
-# Create a file in /var/www/html with holberton school
-file { 'Insert a string in index.html':
+#Added a file index-.html
+file { '/var/www/html/index.html':
   ensure  => present,
-  path    => '/var/www/html/index.html',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => 'Holberton School',
+  content => 'Holberton School'
 }
-->
-# Edit a configuration file to redirect to another page
-exec { 'Edit config nginx file to redirect to another page':
-  command  => 'sed -i "42i \\\n\tlocation /redirect_me {\n\t\t return 301 https://www.youtube.com/watch?v=3MbaGJN2ioQ;\n\t}\n" /etc/nginx/sites-available/default',
-  user     => 'root',
-  provider => 'shell',
+
+#Redirect to another file
+file_line { 'Redirection':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
-->
-# Restart web server
-exec { 'Restart web server':
-  command  => 'service nginx restart'
-  user     => 'root',
-  provider => 'shell',
+
+# Restart service
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Redirection']
 }
