43b7edda9595248ff332cc9b86989228e016889b
el-dani-cortes <danicor83@gmail.com>
2021-04-12 22:04:06 +0000

    UPDATED | Removed some puppet recipe because wasn't ask by the advance task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,27 +5,6 @@ package { 'nginx':
   ensure  => installed,
 }
 
-#  html web page that contains the string Holberton School
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-}
-
-# Add a redirection (/redirect_me) to another page
-file_line { 'redirection site':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=3MbaGJN2ioQ permanent;',
-}
-
-# custom 404 page that contains the string Ceci n'est pas une page.
-file_line { 'custom 404 site':
-  ensure => 'present',
-  path   => '/var/www/html/custom_404.html',
-  after  => '/server_name _;',
-  line   => 'error_page 404 /custom_404.html;\n'
-}
-
 # custom Nginx response header (X-Served-By: hostname)
 file_line { 'add HTTP header':
   ensure => 'present',
@@ -33,6 +12,7 @@ file_line { 'add HTTP header':
   after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;',
 }
+
 # start service
 service { 'nginx':
   ensure  => 'running',
